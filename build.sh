c311982252aa62071b4ad9598a5371fe42549548
marco-costa-mend-support <marco.costa@mend.io>
2025-11-13 13:18:12 +0100

    Add files via upload

---
+++ build.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Build and tag wss-ghe-app
+# docker build -t wss-ghe-app:25.9.1 wss-ghe-app-25.9.1/docker
+docker build -t wss-ghe-app:25.9.1 wss-ghe-app/docker
+
+# Build and tag wss-scanner
+# docker build -t wss-scanner:${wss-scanner.version} wss-scanner-${wss-scanner.version}/docker
+docker build -t wss-scanner:25.9.1.3 wss-scanner/docker
+
+# Build and tag wss-remediate
+# docker build -t wss-remediate:25.9.1 wss-remediate-25.9.1/docker
+docker build -t wss-remediate:25.9.1 wss-remediate/docker
